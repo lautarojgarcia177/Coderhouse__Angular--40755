5b6c7b31a0c35c32f54b80034decdf91747c65b0
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-03-02 16:59:05 -0300

    Comentado

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// JAVASCRIPT REPASO
+
+// En Javascript tenemos datos primitivos:
 const edadSebastian = 23;
 const nombreJose = "Juan Jose Lopez Venegas";
 const jorgeEsIngeniero = true;
-
+// Y objetos que adentro tienen datos u otros objetos:
 const juanSebastian = {
   nombre: "Juan",
   apellido: "Saldaña",
   edad: 25,
   saldoEnElBanco: 2000000,
+  // Para guardar mas de un valor en una propiedad, se utilizan arreglos []
   mascotas: [
     {
       raza: "Criolla",
@@ -21,32 +25,40 @@ const juanSebastian = {
       esMacho: false,
     },
     {
-        raza: "Carey",
-        edad: 4,
-        color: "negro",
-        esMacho: false,
-      },
+      raza: "Carey",
+      edad: 4,
+      color: "negro",
+      esMacho: false,
+    },
   ],
+  // Tambien se pueden guardar funciones en objetos, las funciones son codigo que definimos y ejecutamos cuando lo necesitamos
+  cumplirEdad: function () {
+    juanSebastian.edad = juanSebastian.edad + 1;
+  },
+  cobrarSalario: function () {
+    juanSebastian.saldoEnElBanco *= 2;
+  },
+  // Las funciones pueden recibir parametros, que son datos que va a utilizar para ejecutarse
+  irAlVeterinario: function (precioVeterinario) {
+    // Condicional
+    if (juanSebastian.saldoEnElBanco >= precioVeterinario) {
+      juanSebastian.saldoEnElBanco -= precioVeterinario;
+    } else {
+      console.log("No hay dinero suficiente para ir al veterinario");
+    }
+  },
 };
 
-function cumplirEdad() {
-    juanSebastian.edad = juanSebastian.edad + 1;
-}
+// De esta manera insertamos un objeto en un arreglo
+juanSebastian.mascotas.push({
+  raza: "Payaso",
+  edad: 2,
+  color: "rojo",
+  esMacho: true,
+});
 
-function cobrarSalario() {
-    juanSebastian.saldoEnElBanco *= 2;
-}
-
-function irAlVeterinario() {
-    if (juanSebastian.saldoEnElBanco >= 1000000) {
-        juanSebastian.saldoEnElBanco -= 1000000;
-    } else {
-        console.log('No hay dinero suficiente para ir al veterinario');
-    }
-}
-
-irAlVeterinario();
-irAlVeterinario();
-let esElCumpleDeJuan = false;
+// Asi invocamos funciones, en este caso pasandole un parametro
+juanSebastian.irAlVeterinario(1000000);
+juanSebastian.irAlVeterinario(900000);
 
 console.log(juanSebastian);
